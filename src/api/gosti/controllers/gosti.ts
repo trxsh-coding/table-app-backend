5e2fb9e46416de9f9400e6dcf792a0850f0232ae
Orest Knxwn <orest@200apps.com>
2025-04-30 18:13:12 +0300

    Инциализация

---
+++ src/api/gosti/controllers/gosti.ts
@@ -0,0 +1,7 @@
+/**
+ * gosti controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::gosti.gosti');
---
+++ src/api/gosti/services/gosti.ts
@@ -0,0 +1,7 @@
+/**
+ * gosti service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::gosti.gosti');
